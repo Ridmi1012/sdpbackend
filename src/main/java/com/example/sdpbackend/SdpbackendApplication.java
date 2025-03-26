042e6c9cc19a2868b5123117ba3e9971aa15d201
Ridmi1012 <ridmikaveeshasilva@gmail.com>
2025-03-26 21:52:15 +0530

    Initial commit

---
+++ src/main/java/com/example/sdpbackend/SdpbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sdpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdpbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdpbackendApplication.class, args);
+	}
+
+}
